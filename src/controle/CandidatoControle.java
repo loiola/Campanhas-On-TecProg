dbfa04cd1696eaaf2e23438d20fee34d66ca9735
Augusto Modesto <modestoo@users.noreply.github.com>
2014-08-20 23:28:07 -0300

    Added standards stylesheet to class CandidatoControle

--- src/controle/CandidatoControle.java
+++ src/controle/CandidatoControle.java
@@ -1,22 +1,38 @@
 package controle;
 
 import java.util.LinkedList;
-
 import modelo.beans.Candidato;
 import modelo.dao.CandidatoDAO;
 
 public class CandidatoControle {
-
+	
+	/*
++	 * Control class political party, which mediates the application layer with the model
++	 */
++	
++	// Attributes
 	private CandidatoDAO candidatoDAO;
 
+	// Constructors	
 	public CandidatoControle() {
 		this.candidatoDAO = new CandidatoDAO();
 	}
 	
+	// Other methods
++	/*
++	 * Method that requests a search of all candidate with the name informed
+	 * @param the name of candidate
++	 * @return an LinkedList a list with all candidate with name informed
++	 */
 	public LinkedList<Candidato> getListaCandidatos(String nome) {
 		return this.candidatoDAO.getListaPeloNome(nome);
 	}
 
++	/*
++	 * Method that requests a search of all candidate with the voter informed 
+	 * @param the voter of the candidate
++	 * @return a candidate with voter informed
++	 */
 	public Candidato getUmCandidato(String tituloEleitoral) {
 		return this.candidatoDAO.getCandidatoPeloTitulo(tituloEleitoral);
 	}
