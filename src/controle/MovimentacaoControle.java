65f7dd2a128955f017fdf46d5aa3f8ef7f66543b
Augusto Modesto <modestoo@users.noreply.github.com>
2014-08-21 22:36:43 -0300

    Added standards stylesheet to class MovimentacaoControle

--- src/controle/MovimentacaoControle.java
+++ src/controle/MovimentacaoControle.java
@@ -2,7 +2,6 @@ package controle;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import modelo.beans.Campanha;
 import modelo.beans.Despesa;
 import modelo.beans.Receita;
@@ -11,14 +10,26 @@ import modelo.dao.ReceitaDAO;
 
 public class MovimentacaoControle {
 	
+	/*
+	 * Control class handling financial, which mediates the application layer with the model
+	 */
+
+	// Attributes
 	DespesaDAO despesaDAO;
 	ReceitaDAO receitaDAO;
 	
+	// Constructors
 	public MovimentacaoControle(){
 		this.despesaDAO = new DespesaDAO();
 		this.receitaDAO = new ReceitaDAO();
 	}
 
+	// Other methods
+	/*
+	 * Method that makes the application of a revenue requirement of a campaign 
+	 * @param a campaign
+	 * @return a List with revenues campaign informed
+	 */
 	public List<Receita> getListaReceitas(Campanha campanha) throws Exception {
 
 		ArrayList<Receita> listaReceita = new ArrayList<>();
@@ -42,6 +53,11 @@ public class MovimentacaoControle {
 		return listaReceita;
 	}
 
+	/*
+	 * Method that makes the request for an application of a campaign expense 
+	 * @param a campaign
+	 * @return a List with expenses campaign informed
+	 */
 	public List<Despesa> getListaDespesas(Campanha campanha) throws Exception {
 		
 		ArrayList<Despesa> listaDespesa = new ArrayList<>();
@@ -57,10 +73,20 @@ public class MovimentacaoControle {
 		return listaDespesa;
 	}
 
+	/*
+	 * Method that makes a request of request for a recipe campaign by ID
+	 * @param a ID
+	 * @return the reported income
+	 */
 	public Receita getReceitaPeloId(int id) throws Exception {
 		return this.receitaDAO.getPeloId(id);
 	}
 
+	/*
+	 * Method that makes a request of request for an expenditure campaign by ID 
+	 * @param a ID
+	 * @return the reported expense
+	 */
 	public Despesa getDespesaPeloId(int id) throws Exception {
 		return this.despesaDAO.getPeloId(id);
 	}
