a1a6bfbed2f2775a61692014b3ef4fdbd7cc5e1f
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-08-24 15:56:28 -0300

    Startup documentation control classes of parse

--- src/parse/controle/CampanhaParseControle.java
+++ src/parse/controle/CampanhaParseControle.java
@@ -7,23 +7,40 @@ import modelo.dao.CampanhaDAO;
 import parse.ParseException;
 import parse.indices.IndicesParse;
 
-public class CampanhaParseControle extends ParseControle<Campanha>{
+public class CampanhaParseControle extends ParseControle<Campanha> {
+	
+	/*
+	 * Class used to control comparison and registration of instances of Class Campaign
+	 */
 
+	// Constructors
 	public CampanhaParseControle(IndicesParse<Campanha> indicesParse) {
 		super(indicesParse, new CampanhaDAO());
 	}
 
+	/*
+	 * This method instantiates an object of Class Campaign
+	 * @return an instance of Class Campaign
+	 */
 	@Override
 	public Campanha novaInstancia() {
 		Campanha campanha = new Campanha();
 		return campanha;
 	}
 
+	/*
+	 * This method checks if two instances are equal Class Campaign
+	 * @param two instances of Class Campaign
+	 * @return a boolean value
+	 */
 	@Override
 	public boolean iguais(Campanha objetoUm, Campanha objetoDois) {
 		return objetoUm.equals(objetoDois);
 	}
 	
+	/*
+	 * This method performs the processing and registration of instances of Class Campaign
+	 */
 	@Override
 	public void cadastrarInstancias() throws ParseException {
 		ArrayList<Campanha> listaCampanhas = new ArrayList<>();
