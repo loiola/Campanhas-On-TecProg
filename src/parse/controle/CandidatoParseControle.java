8dabbcd0b67916453765361a215677c02caa21f5
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-08-26 15:11:45 -0300

    Documenting Classes parse the control of Positions and Candidates

--- src/parse/controle/CandidatoParseControle.java
+++ src/parse/controle/CandidatoParseControle.java
@@ -5,17 +5,31 @@ import modelo.dao.CandidatoDAO;
 import parse.indices.IndicesParse;
 
 public class CandidatoParseControle extends ParseControle<Candidato> {
+	
+	/*
+	 * Class used to control comparison and registration of instances of Class Candidate
+	 */
 
+	// Constructors
 	public CandidatoParseControle(IndicesParse<Candidato> indicesParse) {
 		super(indicesParse, new CandidatoDAO());
 	}
 	
+	/*
+	 * This method instantiates an object of Class Candidate
+	 * @return an instance of Class Candidate
+	 */
 	@Override
 	public Candidato novaInstancia() {
 		Candidato candidato = new Candidato();
 		return candidato;
 	}
 
+	/*
+	 * This method checks if two instances are equal Class Candidate
+	 * @param two instances of Class Candidate
+	 * @return a boolean value
+	 */
 	@Override
 	public boolean iguais(Candidato objetoUm, Candidato objetoDois) {
 		return objetoUm.equals(objetoDois);
--- src/parse/controle/CargoParseControle.java
+++ src/parse/controle/CargoParseControle.java
@@ -5,17 +5,31 @@ import modelo.dao.CargoDAO;
 import parse.indices.IndicesParse;
 
 public class CargoParseControle extends ParseControle<Cargo> {
+	
+	/*
+	 * Class used to control comparison and registration of instances of Class Position
+	 */
 
+	// Constructors
 	public CargoParseControle(IndicesParse<Cargo> indicesParse) {
 		super(indicesParse, new CargoDAO());
 	}
 
+	/*
+	 * This method instantiates an object of Class Position
+	 * @return an instance of Class Position
+	 */
 	@Override
 	public Cargo novaInstancia() {
 		Cargo cargo = new Cargo();
 		return cargo;
 	}
 
+	/*
+	 * This method checks if two instances are equal Class Position
+	 * @param two instances of Class Position
+	 * @return a boolean value
+	 */
 	@Override
 	public boolean iguais(Cargo objetoUm, Cargo objetoDois) {
 		return objetoUm.equals(objetoDois);
