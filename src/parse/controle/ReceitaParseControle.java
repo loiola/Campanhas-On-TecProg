8aff4a6900383a27e351484de25cb8f313fe9eb5
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-08-27 14:57:53 -0300

    Completion of documentation control classes of parse

--- src/parse/controle/ReceitaParseControle.java
+++ src/parse/controle/ReceitaParseControle.java
@@ -5,17 +5,31 @@ import modelo.dao.ReceitaDAO;
 import parse.indices.IndicesParse;
 
 public class ReceitaParseControle extends ParseControle<Receita> {
+	
+	/*
+	 * Class used to control comparison and registration of instances of Class Revenue
+	 */
 
+	// Constructors
 	public ReceitaParseControle(IndicesParse<Receita> indicesParse) {
 		super(indicesParse, new ReceitaDAO());
 	}
 
+	/*
+	 * This method instantiates an object of Class Revenue
+	 * @return an instance of Class Revenue
+	 */
 	@Override
 	public Receita novaInstancia() {
 		Receita receita = new Receita();
 		return receita;
 	}
 
+	/*
+	 * This method checks if two instances are equal Class Revenue
+	 * @param two instances of Class Revenue
+	 * @return a boolean value
+	 */
 	@Override
 	public boolean iguais(Receita objetoUm, Receita objetoDois) {
 		return objetoUm.equals(objetoDois);
--- src/parse/controle/ResultadoParseControle.java
+++ src/parse/controle/ResultadoParseControle.java
@@ -5,17 +5,31 @@ import modelo.dao.ResultadoDAO;
 import parse.indices.IndicesParse;
 
 public class ResultadoParseControle extends ParseControle<Resultado> {
+	
+	/*
+	 * Class used to control comparison and registration of instances of Class Result
+	 */
 
+	// Constructors
 	public ResultadoParseControle(IndicesParse<Resultado> indicesParse) {
 		super(indicesParse, new ResultadoDAO());
 	}
 
+	/*
+	 * This method instantiates an object of Class Result
+	 * @return an instance of Class Result
+	 */
 	@Override
 	public Resultado novaInstancia() {
 		Resultado resultado = new Resultado();
 		return resultado;
 	}
 
+	/*
+	 * This method checks if two instances are equal Class Result
+	 * @param two instances of Class Result
+	 * @return a boolean value
+	 */
 	@Override
 	public boolean iguais(Resultado objetoUm, Resultado objetoDois) {
 		return objetoUm.equals(objetoDois);
