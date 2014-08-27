ae58180c43efb8afcfdea5058797e291ffdcbc1a
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-08-27 14:50:34 -0300

    Documentation of the abstract class control and documentation parse other classes of control parse

--- src/parse/controle/ParseControle.java
+++ src/parse/controle/ParseControle.java
@@ -7,13 +7,18 @@ import parse.ParseException;
 import parse.indices.IndicesParse;
 
 public abstract class ParseControle<O> {
+	
+	/*
+	 * Abstract class for template methods to be used by subclasses to parse control
+	 */
 
+	// Attributes
 	private O objetoVazio;
 	private ParseDAO<O> basicoDAO;
 	private IndicesParse<O> indicesParse;
-	
 	protected ArrayList<O> listaInstancias;
 	
+	// Constructors
 	public ParseControle(IndicesParse<O> indicesParse, ParseDAO<O> basicoDAO) {
 		this.listaInstancias = new ArrayList<>();
 
@@ -22,35 +27,59 @@ public abstract class ParseControle<O> {
 		this.objetoVazio = novaInstancia();
 	}
 
+	// Method signature to make a new instance of an object of any class
 	public abstract O novaInstancia();
+	
+	// Signature of the method to compare two objects of any class
 	public abstract boolean iguais(O objetoUm, O objetoDois);
 	
+	/*
+	 * This method adds an instance of a class in the list of instances
+	 * @param an array of strings
+	 */
 	public void addInstancia(String campo[]) {
 		O objeto = fazerNovaInstancia(campo);		
-		if( (!iguais(objeto, objetoVazio)) && 
-				(!this.listaInstancias.contains(objeto)) ) {
+		if((!iguais(objeto, objetoVazio)) && 
+				(!this.listaInstancias.contains(objeto))) {
 			this.listaInstancias.add(objeto);
 		}
 	}
 
+	/*
+	 * This method performs addition of instances without performing validation of existence 
+	 * in the list of instances
+	 * @param an array of strings
+	 */
 	public void addInstanciaIgual(String campo[]) {
 		O objeto = fazerNovaInstancia(campo);
-		if( (!iguais(objeto, objetoVazio)) ) {
+		if((!iguais(objeto, objetoVazio))) {
 			this.listaInstancias.add(objeto);
 		}
 	}
 	
+	/*
+	 * This method formalizes the registration of a list of instances
+	 */
 	public void cadastrarInstancias() throws ParseException {
 		this.basicoDAO.cadastrarListaParse(this.listaInstancias);
 	}
 	
+	/*
+	 * This method performs a cleaning at the array of instances to register
+	 */
 	public void resetar() {
 		this.listaInstancias.clear();
 	}
 	
+	/*
+	 * This method makes a new instance and returns the instantiated object
+	 * @param an array of strings
+	 * @return an object of any class
+	 */
 	private O fazerNovaInstancia(String campo[]) {
 		O objeto = novaInstancia();
 		this.indicesParse.iniciarInstancia(objeto, campo);
 		return objeto;
 	}
+	
 }
--- src/parse/controle/PartidoParseControle.java
+++ src/parse/controle/PartidoParseControle.java
@@ -5,17 +5,31 @@ import modelo.dao.PartidoDAO;
 import parse.indices.IndicesParse;
 
 public class PartidoParseControle extends ParseControle<Partido> {
+	
+	/*
+	 * Class used to control comparison and registration of instances of Class Party
+	 */
 
+	// Constructors
 	public PartidoParseControle(IndicesParse<Partido> indicesParse) {
 		super(indicesParse, new PartidoDAO());
 	}
 
+	/*
+	 * This method instantiates an object of Class Party
+	 * @return an instance of Class Party
+	 */
 	@Override
 	public Partido novaInstancia() {
 		Partido partido = new Partido();
 		return partido;
 	}
 
+	/*
+	 * This method checks if two instances are equal Class Party
+	 * @param two instances of Class Party
+	 * @return a boolean value
+	 */
 	@Override
 	public boolean iguais(Partido objetoUm, Partido objetoDois) {
 		return objetoUm.equals(objetoDois);
