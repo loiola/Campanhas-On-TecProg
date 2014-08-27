6ae42c4a8e67127c515c0111c8936697452934ca
Augusto Modesto <modestoo@users.noreply.github.com>
2014-08-26 22:31:43 -0300

    Added standards stylesheet to interface Logica

--- src/controle/servlet/Logica.java
+++ src/controle/servlet/Logica.java
@@ -4,6 +4,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Logica {
+    
+	/*
+	 * Interface to implement the logic of dynamic display of indices of pages
+	 */
+
+	// Method Signatures
+	/*
+	 * Method signatures to implement the logic of dynamic display of indices of pages
+	 * @param an HTTP request
+	 * @return an String
+	 */
     String executa(HttpServletRequest req,
             HttpServletResponse res) throws Exception;    
 }
