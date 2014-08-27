bf90170bd44eba66ec2662ae908cae390a94120c
Augusto Modesto <modestoo@users.noreply.github.com>
2014-08-26 22:56:44 -0300

    Added standards stylesheet to servlet SelecionarCandidato

--- src/controle/servlet/SelecionarCandidato.java
+++ src/controle/servlet/SelecionarCandidato.java
@@ -9,12 +9,18 @@ import modelo.beans.Campanha;
 import modelo.beans.Candidato;
 import modelo.beans.Despesa;
 import modelo.beans.Receita;
+
 import controle.CampanhaControle;
 import controle.CandidatoControle;
 import controle.MovimentacaoControle;
 
 public class SelecionarCandidato implements Logica {
+	
+	/*
+	 * Servlet requests to control the selection of political candidates
+	 */
 
+	// Attributes
 	private CandidatoControle candidatoControle;
 	private Candidato candidato;
 	private CampanhaControle campanhaControle;
@@ -39,6 +45,14 @@ public class SelecionarCandidato implements Logica {
 	private float despesaCalc2010;
 	private float receitaCalc2010;
 
+	
+	// Other methods
+	/*
+	 * Concretizing method that implements requests for selection of a
+	 * candidate in all their years of election campaign 
+	 * @param an HTTP request and HTTP response
+	 * @return the next HTML page and their responses to requests
+	 */
 	@Override
 	public String executa(HttpServletRequest req, HttpServletResponse res)
 			throws Exception {
@@ -57,8 +71,11 @@ public class SelecionarCandidato implements Logica {
 
 		this.listaCampanha = this.campanhaControle.getListaCampanhas(this.candidato);
 		
+		// Loop to receive all the necessary information about their candidate campaigns
 		for(Campanha campanha : listaCampanha) {
-			if(campanha.getAno()==2002) {
+			
+			// Condition for receiving data from the 2002 campaign if there
+			if(campanha.getAno() == 2002) {
 				this.listaReceita = 
 						this.movimentacaoControle.getListaReceitas(campanha);
 				this.listaDespesa = 
@@ -72,8 +89,9 @@ public class SelecionarCandidato implements Logica {
 				campanha.setDespesaTotalCalculada(this.despesaCalc2002);
 				campanha.setReceitaTotalCalculada(this.receitaCalc2002);
 			}
-
-			else if(campanha.getAno()==2006) {
+			
+			// Condition for receiving data from the 2006 campaign if there
+			else if(campanha.getAno() == 2006) {
 				this.listaReceita = 
 						this.movimentacaoControle.getListaReceitas(campanha);
 				this.listaDespesa = 
@@ -83,12 +101,13 @@ public class SelecionarCandidato implements Logica {
 					this.receitaCalc2006 += receita.getValor();
 				for(Despesa despesa : listaDespesa)
 					this.despesaCalc2006 += despesa.getValor();
-				
 
 				campanha.setDespesaTotalCalculada(this.despesaCalc2006);
 				campanha.setReceitaTotalCalculada(this.receitaCalc2006);
 			}
-			else if(campanha.getAno()==2010) {
+			
+			// Condition for receiving data from the 2010 campaign if there
+			else if(campanha.getAno() == 2010) {
 				this.listaReceita = 
 						this.movimentacaoControle.getListaReceitas(campanha);
 				this.listaDespesa = 
@@ -104,9 +123,9 @@ public class SelecionarCandidato implements Logica {
 			}
 		}
 		
+		// Set of answers to requests made concerning the applicant requested
 		req.setAttribute("candidato", this.candidato);
 		req.setAttribute("campanhas", this.listaCampanha);
-		
 		req.setAttribute("candidato", this.candidato);
 		req.setAttribute("campanhas", this.listaCampanha);
 		req.setAttribute("despesa1", this.despesaMax2002);
@@ -118,8 +137,8 @@ public class SelecionarCandidato implements Logica {
 		req.setAttribute("receita2", this.receitaCalc2006);
 		req.setAttribute("despesa03", this.despesaCalc2010);
 		req.setAttribute("receita3", this.receitaCalc2010);
-
+		
+		// Return the HTML page with the requested information
 		return "/visualizar_candidato.jsp";
 	}
-
 }
