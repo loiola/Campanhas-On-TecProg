cc67d26ab608888e7a31f0e84615b986b25a1f20
vannessasoares <vannessa.andrade@gmail.com>
2014-08-29 15:01:50 -0300

    Adding documentation comments in Receita.java

--- src/modelo/beans/Receita.java
+++ src/modelo/beans/Receita.java
@@ -1,18 +1,27 @@
 package modelo.beans;
 
 public class Receita extends MovimentacaoFinanceira {
-	
+
+	/*
+	 *  Class Receita.java
+	 * This class is responsable for getting the Receipts's informations
+	 */
+	 
+	// Constants 
 	public static final Doador DOADOR_VAZIO = new Doador();
 	
+	// Attributes
 	private String reciboEleitoral;
 	private Doador doador;
 	
+	// Empty Constructor
 	public Receita() {
 		super();
 		this.reciboEleitoral = STRING_VAZIO;
 		this.doador = DOADOR_VAZIO;
 	}
 	
+	// Getters and Setters
 	public String getReciboEleitoral() {
 		return reciboEleitoral;
 	}
