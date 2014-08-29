f0bdf94122257bab608c880e1c3cbed07c72b66b
vannessasoares <vannessa.andrade@gmail.com>
2014-08-29 14:53:14 -0300

    Adding documentation comments in Resultado. java

--- src/modelo/beans/Resultado.java
+++ src/modelo/beans/Resultado.java
@@ -2,26 +2,26 @@ package modelo.beans;
 
 public class Resultado {
 
+	/*
+	 * Class Resultado.java
+	 * This class is responsable for getting the Result's informations
+	 */
+	
+	// Constants
 	public static final String STRING_VAZIO = "";
 	public static final int INTEGER_VAZIO = 0;
 	
+	// Attributes
 	private Integer codigo;
 	private String  descricao;
 	
+	// Empty constructor
 	public Resultado() {
 		this.codigo = INTEGER_VAZIO;
 		this.descricao = STRING_VAZIO;
 	}
 	
-	@Override
-	public boolean equals(Object object) {
-		if( !(object instanceof Resultado))
-			return false;
-		
-		Resultado outroResultado = (Resultado) object;
-		return this.descricao.equalsIgnoreCase(outroResultado.getDescricao());
-	}
-	
+	// Getters and Setters
 	public Integer getCodigo() {
 		return codigo;
 	}
@@ -35,4 +35,12 @@ public class Resultado {
 		this.descricao = descricao;
 	}
 	
+	@Override
+	public boolean equals(Object object) {
+		if( !(object instanceof Resultado))
+			return false;
+		
+		Resultado outroResultado = (Resultado) object;
+		return this.descricao.equalsIgnoreCase(outroResultado.getDescricao());
+	}
 }
