c39c2b8bc224a681a858d65f7e8d94c2a65ae2a2
Augusto Modesto <modestoo@users.noreply.github.com>
2014-08-30 13:55:03 -0300

    Added standards stylesheet to class ServletControle

--- src/controle/servlet/ServletControle.java
+++ src/controle/servlet/ServletControle.java
@@ -10,12 +10,21 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/mvc")
 public class ServletControle extends HttpServlet {
+	
+	/*
+	 * Controller servlet instance
+	 */
 
+	// Attributes
 	private static final long serialVersionUID = 2663398443592480668L;
-
+	
+	// Other methods
+	/*
+	 * Method that checks the instance and return an exception or resumes the execution
+	 * @param an HTTP request and HTTP response
+	 */
 	protected void service(HttpServletRequest request,
-			HttpServletResponse response) 
-					throws ServletException, IOException {
+			HttpServletResponse response) throws ServletException, IOException {
 
 		String parametro = request.getParameter("logica");
 		String nomeDaClasse = "controle.servlet." + parametro;
@@ -28,7 +37,7 @@ public class ServletControle extends HttpServlet {
 
 			request.getRequestDispatcher(pagina).forward(request, response);
 
-		} catch (Exception e) {
+		} catch(Exception e) {
 			throw new ServletException(
 					"A lógica de negócios causou uma exceção", e);
 		}
