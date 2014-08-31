dfdcc721969758542aed3c37a69fb57139bda1a9
Arkye <arkye@hotmail.com.br>
2014-08-30 21:17:12 -0300

    Applying Style Sheet to ParseCampaignRegister and ParseCandidateRegister Classes
    Commentary in methods, attributes and a description of the class

--- src/parse/cadastro/campanha/CadastroCandidatoParse.java
+++ src/parse/cadastro/campanha/CadastroCandidatoParse.java
@@ -8,13 +8,31 @@ import parse.controle.ParseControle;
 import parse.indices.CandidatoIndicesParse;
 import parse.indices.IndicesParse;
 
+/* 
+ * Class used to extract Candidate attributes and forward the register to the Database
+ */
 public class CadastroCandidatoParse extends CadastroParse<Candidato> {
 
+	// Constructor
+	
+	/*
+	 * This constructor use the ParseRegister inherited constructor to
+	 * register informations from a Candidate
+	 * @param String who define the type of the list file to be used to get the ParseIndex
+	 * @param String who define the year of the campaign to be used to get the ParseIndex
+	 */
 	public CadastroCandidatoParse(String tipoArquivo, String ano)
 			throws ParseException {
 		super(tipoArquivo, ano);
 	}
 
+	// Methods
+	
+	/*
+	 * @see parse.cadastro.CadastroParse#novaInstancia(parse.indices.IndicesParse)
+	 * This method generate a ParseCandidateControl to be used by constructor
+	 * @return a ParseCandidateControl
+	 */
 	@Override
 	public ParseControle<Candidato> novaInstancia(
 			IndicesParse<Candidato> indicesParse) {
@@ -22,6 +40,11 @@ public class CadastroCandidatoParse extends CadastroParse<Candidato> {
 		return candidatoParseControle;
 	}
 
+	/*
+	 * @see parse.cadastro.CadastroParse#getIndicesParse(java.lang.String, java.lang.String)
+	 * This method generate the ParseCandidateIndex, setting the index number for each attribute
+	 * @return a ParseCandidateIndex
+	 */
 	@Override
 	protected IndicesParse<Candidato> getIndicesParse(String tipoArquivo,
 			String ano) throws ParseException {
@@ -35,5 +58,4 @@ public class CadastroCandidatoParse extends CadastroParse<Candidato> {
 		return candidatoIndicesParse;
 	}
 	
-
 }
