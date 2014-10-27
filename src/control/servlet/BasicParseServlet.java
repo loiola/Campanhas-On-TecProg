1568e50bd6e96ce88d5f430a2fed1edc219a3d94
arkye <arkye@hotmail.com.br>
2014-10-27 00:56:11 -0200

    Refactoring Parses Servlets
    From Servlet Control Package

---
+++ src/control/servlet/BasicParseServlet.java
@@ -0,0 +1,201 @@
+package control.servlet;
+
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import parse.Parse;
+import parse.ParseParty;
+
+public class BasicParseServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = -2550694521233885904L;
+	
+	private final String INFORM_PARSE_FINISHED = "Parse Completed!";
+	private final String EXCEPTION_ERROR = "ERROR test upload: ";
+	private final String FILE_LINE_INITIAL = "file_line_initial";
+	private final String INFORM_INITIAL_LINE = "initial_line: ";
+	private final String DIVISION_STRING = ";";
+	private final Integer INITIAL_LINE = 1;
+	private final Part EMPTY_REQUEST_PART = null;
+	
+	private String fileType;
+	private HttpServletRequest servletRequest;
+	private HttpServletResponse servletResponse;
+	private PrintWriter responseOutput;
+	private Part requestPart;
+	private Scanner servletScanner;
+	private FileItemFactory fileItemFactory;
+	private ServletFileUpload servletFileUpload;
+	private List<FileItem> fileFields;
+	private FileItem fileItem;
+	private Parse parse;
+	
+	// Init execute method from Super (HttpServlet)
+	@Override
+	public void init() throws ServletException {
+		super.init();
+	}
+	
+	protected void readDataFile(HttpServletRequest request, HttpServletResponse response, String fileType) {
+		try {
+			setAttributes(request,response,fileType);
+			scanDataFile();
+			extractDataInfo();
+			generateFields();
+			callControlToRunParse();
+			printFinishParsingToUser();
+		} catch (Exception e) {
+			getResponseOutput().println(EXCEPTION_ERROR + e.getMessage());
+		}
+	}
+	
+	private void scanDataFile() throws Exception {
+		setResponseOutput(getServletResponse().getWriter());
+		setRequestPart(getServletRequest().getPart(FILE_LINE_INITIAL));
+		if (getRequestPart() != EMPTY_REQUEST_PART) {
+			setServletScanner(new Scanner(getRequestPart().getInputStream()));
+			getResponseOutput().println(INFORM_INITIAL_LINE + getServletScanner().nextLine());
+			getServletScanner().close();
+		}
+		else {
+			// TODO Maybe: Inform the Request Part is Empty
+		}
+	}
+	
+	private void extractDataInfo() throws Exception {
+		setFileItemFactory(new DiskFileItemFactory());
+		setServletFileUpload(new ServletFileUpload(getFileItemFactory()));
+		setFileFields(getServletFileUpload().parseRequest(getServletRequest()));
+	}
+	
+	private void generateFields() {
+		for(FileItem fileItemPointer : getFileFields()) {
+			if(!fileItemPointer.isFormField()) {
+				setFileItem(fileItemPointer);
+			}
+		}		
+	}
+	
+	private void printFinishParsingToUser() {
+		getResponseOutput().println(INFORM_PARSE_FINISHED);
+		
+	}
+	
+	private void callControlToRunParse() throws Exception {
+		setParse();
+		getParse().runParse(getFileItem(), DIVISION_STRING, INITIAL_LINE);
+	}
+	
+	private void setAttributes(HttpServletRequest request, HttpServletResponse response, String fileType) {
+		setServletRequest(request);
+		setServletResponse(response);
+		setFileType(fileType);
+	}
+	
+	private String getFileType() {
+		return fileType;
+	}
+
+	private void setFileType(String fileType) {
+		this.fileType = fileType;
+	}
+
+	private HttpServletRequest getServletRequest() {
+		return servletRequest;
+	}
+
+	private void setServletRequest(HttpServletRequest servletRequest) {
+		this.servletRequest = servletRequest;
+	}
+
+	private HttpServletResponse getServletResponse() {
+		return servletResponse;
+	}
+
+	private void setServletResponse(HttpServletResponse servletResponse) {
+		this.servletResponse = servletResponse;
+	}
+
+	private PrintWriter getResponseOutput() {
+		return responseOutput;
+	}
+
+	private void setResponseOutput(PrintWriter responseOutput) {
+		this.responseOutput = responseOutput;
+	}
+
+	private Part getRequestPart() {
+		return requestPart;
+	}
+
+	private void setRequestPart(Part requestPart) {
+		this.requestPart = requestPart;
+	}
+
+	private Scanner getServletScanner() {
+		return servletScanner;
+	}
+
+	private void setServletScanner(Scanner servletScanner) {
+		this.servletScanner = servletScanner;
+	}
+
+	private FileItemFactory getFileItemFactory() {
+		return fileItemFactory;
+	}
+
+	private void setFileItemFactory(FileItemFactory fileItemFactory) {
+		this.fileItemFactory = fileItemFactory;
+	}
+
+	private ServletFileUpload getServletFileUpload() {
+		return servletFileUpload;
+	}
+
+	private void setServletFileUpload(ServletFileUpload servletFileUpload) {
+		this.servletFileUpload = servletFileUpload;
+	}
+
+	private List<FileItem> getFileFields() {
+		return fileFields;
+	}
+
+	private void setFileFields(List<FileItem> fileFields) {
+		this.fileFields = fileFields;
+	}
+
+	private FileItem getFileItem() {
+		return fileItem;
+	}
+
+	private void setFileItem(FileItem fileItem) {
+		this.fileItem = fileItem;
+	}
+
+	private Parse getParse() {
+		return parse;
+	}
+
+	private void setParse() throws Exception {
+		if(getFileType() == "party") {
+			this.parse = new ParseParty(getFileType(), "");
+		}
+		else {
+			this.parse = null;
+		}
+	}
+	
+}
+
--- src/control/servlet/LoadPoliticalPartyParse.java
+++ src/control/servlet/LoadPoliticalPartyParse.java
@@ -1,27 +1,11 @@
 package control.servlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
-import java.util.Scanner;
-
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
-
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileItemFactory;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
-
-import parse.Parse;
-import parse.ParseParty;
 
 @WebServlet("/loadPoliticalPartyParse")
-public class LoadPoliticalPartyParse extends HttpServlet {
+public class LoadPoliticalPartyParse extends BasicParseServlet {
 
 	/*
 	 * Servlet asked by parseView.jsp to extract a Data File of Political
@@ -29,119 +13,20 @@ public class LoadPoliticalPartyParse extends HttpServlet {
 	 * information extracted in the database
 	 */
 
-	// Attribute
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -5159983967292605199L;
 
-	// Constructor execute init() method from Super (HttpServlet)
-	@Override
-	public void init() throws ServletException {
-		super.init();
-	}
-
-	// Other method
-
+	private final String PARTY_PARSE_NAME = "party";
+	
 	/**
 	 * 
 	 * Method that makes the call load and parse controls its execution
 	 * 
 	 * @param request
 	 * @param response
-	 * @throws IOException
-	 * @throws ServletException
 	 */
 	@Override
 	protected void service(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-
-		/*
-		 * General Class to be Instantiated Suggested: BasicParseServlet
-		 */
-		/*
-		 * [1] GENERAL METHOD SUGGESTED: BasicParseServlet.readDataFile() throws
-		 * Servlet Exception, IOException
-		 */
-		/*
-		 * request and response is taken by Attributes from that Class (set)
-		 */
-
-		/*
-		 * [2] GENERAL METHOD SUGGESTED: BasicParseServlet.scanDataFile()
-		 */
-		/*
-		 * output as Attribute (Better name?)
-		 */
-		PrintWriter output = response.getWriter();
-
-		/*
-		 * file_line_initital as Constant (Better name?) "initital line: "
-		 * String as Constant
-		 */
-		Part part = request.getPart("file_line_initial");
-		if (part != null) {
-			Scanner scanner = new Scanner(part.getInputStream());
-			output.println("initial line: " + scanner.nextLine());
-			scanner.close();
-		}
-		// [2] END SUGGESTION
-
-		/*
-		 * GENERAL METHOD SUGGESTED: BasicParseServlet.extractDataInfo(String
-		 * fileType)
-		 */
-		try {
-			boolean isMultpart = ServletFileUpload.isMultipartContent(request);
-			// This if/else is really necessary?
-			if (isMultpart) {
-				/*
-				 * factory, upload, fields, file, filetype (settable) as
-				 * Attributes initialLine as Constant
-				 */
-				FileItemFactory factory = new DiskFileItemFactory();
-				ServletFileUpload upload = new ServletFileUpload(factory);
-
-				List<FileItem> fields = upload.parseRequest(request);
-
-				FileItem file = null;
-				int initialLine = 1;
-				String fileType = "party";
-
-				/*
-				 * [3] GENERAL METHOD SUGGESTED:
-				 * BasicParseServlet.generateFields()
-				 */
-				for (FileItem fileItem : fields) {
-					if (!fileItem.isFormField()) {
-						file = fileItem;
-					}
-				}
-				// [3] END SUGGESTION
-
-				/*
-				 * [4] GENERAL METHOD SUGGESTED:
-				 * BasicParseServlet.callControlToRunParse() [4.1] Additional
-				 * Method Suggested: getParseType()
-				 */
-				/*
-				 * division as Constant deprecated second parameter from
-				 * ParseParty Constructor
-				 */
-				String division = ";";
-				Parse parse = new ParseParty(fileType, "");
-				parse.runParse(file, division, initialLine);
-				// [4] END SUGGESTION
-				/*
-				 * [5] GENERAL METHOD SUGGESTED: printFinishParsingToUser()
-				 */
-				output.println("Parse Completed!");
-				// [5] END SUGGESTION
-			}
-			/*
-			 * Insert Void Else here
-			 */
-		} catch (Exception e) {
-			// Maybe a better Exception Diagnosis
-			output.println("ERROR test upload: " + e.getMessage());
-		}
+			HttpServletResponse response) {
+		readDataFile(request,response,PARTY_PARSE_NAME);
 	}
-	// [1] END SUGGESTION
 }
